6e633b71a867361adf74d2a59b5a4a24fc7be2b7
Jonathan <jonathan.george.dev@gmail.com>
2020-10-10 15:06:04 -0700

    authentication and user info pulled

--- src/App.js
+++ src/App.js
@@ -6,23 +6,34 @@ import Login from "./components/Login";
 import SideBar from "./components/Sidebar";
 import { selectUser } from "./features/userSlice";
 import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 function App() {
   //this is a redux hook
   //look at feature/userslice for more details
   const user = useSelector(selectUser);
 
+  //this allows us to shoot our code into the data layer
   const dispatch = useDispatch();
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      //console.log("user is ", authUser);
+      //console.log("user is ", authUser); if you look are the object it shows all the user info we can use
       if (authUser) {
         //user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
       } else {
         //user is logged out
+        dispatch(logout());
       }
     });
-  }, []);
+  }, [dispatch]); //this takes care of dependency warning
   return (
     //BEM convention used
     <div className="app">
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,8 +10,13 @@ import MicIcon from "@material-ui/icons/Mic";
 import HeadsetIcon from "@material-ui/icons/Headset";
 import SettingsIcon from "@material-ui/icons/Settings";
 import { Avatar } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 
 function Sidebar() {
+  //this is how we are going to pull in our user info from data layer
+  const user = useSelector(selectUser);
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
@@ -50,10 +55,11 @@ function Sidebar() {
         </div>
       </div>
       <div className="sidebar__profile">
-        <Avatar src="https://avatars3.githubusercontent.com/u/32345015?s=460&amp;u=9420ddbb5f4905c861ed60fdf586dcaa7e7e88cf&amp;v=4" />
+        {/*<Avatar src="https://avatars3.githubusercontent.com/u/32345015?s=460&amp;u=9420ddbb5f4905c861ed60fdf586dcaa7e7e88cf&amp;v=4" /> */}
+        <Avatar onClick={} src={user.photo} />
         <div className="sidebar__profileInfo">
-          <h3>b.abasd fds</h3>
-          <p>#thifosd MyID</p>
+          <h3>{user.displayName}</h3>
+          <p>#{user.uid.substring(1, 5)}</p>
         </div>
         <div className="sidebar__profileIcons">
           <MicIcon />
